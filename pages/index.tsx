0b38694a24eda3fb4307b35ab5b468534db44cde
Adriana Morales <amorales25@gmail.com>
2023-01-24 11:59:51 -0500

    added create and ts

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import fs from "fs";
+import matter from "gray-matter";
+import Link from "next/link";
+import path from "path";
+import { postFilePaths, POSTS_PATH } from "../utils/mdxUtils";
+
+export default function Home({ posts }) {
+	return (
+		<div>
+			<h1>Blog It</h1>
+			<p>Create a new post:</p>
+		</div>
+	);
+}
+
+export function getStaticProps() {
+	const posts = postFilePaths.map((filePath) => {
+		const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
+		const { content, data } = matter(source);
+
+		return {
+			content,
+			data,
+			filePath,
+		};
+	});
+
+	return { props: { posts } };
+}
